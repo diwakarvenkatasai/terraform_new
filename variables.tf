2653a90246bcfa4a8675431e6dfab1499b94b132
ramnivas <ramnivasvundavalli.11@gmail.com>
2025-02-21 18:46:31 +0530

    Add new feature

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "ami_id" {
+    description = "AMI id"
+    type = string
+    default = ""
+  
+}
+
+variable "type" {
+    description= "Type of instance"
+    type = string
+    default = ""
+  
+}
+
+variable "key" {
+    description="Key Name(.Pem File)"
+    type = string
+    default = ""
+  
+}
